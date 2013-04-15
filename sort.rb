90e71afaba5629fec70227cece5c538ea0b4e94c
Evgeny Pavlov <evgeny@pavlov.name>
2013-04-15 23:17:47 +0400

    Type of distortion. Move into folders.

--- sort.rb
+++ sort.rb
@@ -1,13 +1,16 @@
 #!/usr/bin/env ruby
 
+require 'fileutils'
+
 # Number of output images in a single folder
 IMAGE_FOLDER_SIZE = 30
 
 # Total number of images
 IMAGE_SET_SIZE = Dir.glob("input/**/*.jpg").select { |file| File.file?(file) }.count
 
-(IMAGE_SET_SIZE / IMAGE_FOLDER_SIZE.to_f).ceil.times do
-  already_taken = []
+(IMAGE_SET_SIZE / IMAGE_FOLDER_SIZE.to_f).ceil.times do |i|
+  already_taken   = []
+  distortion_used = Hash.new { |hash, key| hash[key] = 0 }
 
   Dir.glob("input/**/*.jpg").each do |file|
     # next if item == '.' or item == '..'
@@ -22,7 +25,17 @@ IMAGE_SET_SIZE = Dir.glob("input/**/*.jpg").select { |file| File.file?(file) }.c
     # Second part is number of image. Do not put the same image into a single folder
     next if already_taken.include?(parts[1])
 
+    # Each type of distortion should be used no more than 5 times
+    next if distortion_used[part[2]] > 5
 
+    # Create a folder
+    new_folder = Dir.mkdir("output/#{i}")
 
+    # Move file into a folder
+    FileUtils.move file, new_folder
+
+    # Update arrays and hashes
+    already_taken << parts[1]
+    distortion_used[part[2]] += 1
   end
 end
