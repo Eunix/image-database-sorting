b865cf803fde3da02089b228e670a475c3e13a6f
Evgeny Pavlov <evgeny@pavlov.name>
2013-04-15 23:22:53 +0400

    Updated information about real filename

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Image Database Sorting
 ======================
 
-A sorting images tool for Yaroslavl State University expirement. To use this command-line tool put your unsorted database into `input` folder and type in console
+A sorting images tool for Yaroslavl State University expirement. Assumed that filename is like `female_asian_11_Blur_23.bmp`. To use this command-line tool put your unsorted database into `input` folder and type in console
 
 ```
 ./sort.rb
--- sort.rb
+++ sort.rb
@@ -6,7 +6,7 @@ require 'fileutils'
 IMAGE_FOLDER_SIZE = 30
 
 # Total number of images
-IMAGE_SET_SIZE = Dir.glob("input/**/*.jpg").select { |file| File.file?(file) }.count
+IMAGE_SET_SIZE = Dir.glob("input/**/*.bmp").select { |file| File.file?(file) }.count
 
 (IMAGE_SET_SIZE / IMAGE_FOLDER_SIZE.to_f).ceil.times do |i|
   already_taken   = []
@@ -15,18 +15,19 @@ IMAGE_SET_SIZE = Dir.glob("input/**/*.jpg").select { |file| File.file?(file) }.c
   Dir.glob("input/**/*.jpg").each do |file|
     # next if item == '.' or item == '..'
 
-    # Parts of filename:
-    # 1. Male/female
+    # Parts of filename (female_asian_11_Blur_23)
+    # 0. Male/female
+    # 1. Race
     # 2. Number in the set
     # 3. Type of distortion
     # 4. Level of distortion
     parts = File.basename(file).split("_")
 
     # Second part is number of image. Do not put the same image into a single folder
-    next if already_taken.include?(parts[1])
+    next if already_taken.include?(parts[2])
 
     # Each type of distortion should be used no more than 5 times
-    next if distortion_used[part[2]] > 5
+    next if distortion_used[part[3]] > 5
 
     # Create a folder
     new_folder = Dir.mkdir("output/#{i}")
