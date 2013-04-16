7f4322522bd169d96d0486ed156b734a12ab2ab4
Evgeny Pavlov <evgeny@pavlov.name>
2013-04-16 11:24:20 +0400

    Some bug fixes. Use of two images types

--- sort.rb
+++ sort.rb
@@ -6,13 +6,13 @@ require 'fileutils'
 IMAGE_FOLDER_SIZE = 30
 
 # Total number of images
-IMAGE_SET_SIZE = Dir.glob("input/**/*.bmp").select { |file| File.file?(file) }.count
+IMAGE_SET_SIZE = Dir.glob("input/**/*.{bmp,jp2}").select { |file| File.file?(file) }.count
 
 (IMAGE_SET_SIZE / IMAGE_FOLDER_SIZE.to_f).ceil.times do |i|
   already_taken   = []
   distortion_used = Hash.new { |hash, key| hash[key] = 0 }
 
-  Dir.glob("input/**/*.jpg").each do |file|
+  Dir.glob("input/**/*.{bmp,jp2}").each do |file|
     # next if item == '.' or item == '..'
 
     # Parts of filename (female_asian_11_Blur_23)
@@ -27,16 +27,20 @@ IMAGE_SET_SIZE = Dir.glob("input/**/*.bmp").select { |file| File.file?(file) }.c
     next if already_taken.include?(parts[2])
 
     # Each type of distortion should be used no more than 5 times
-    next if distortion_used[part[3]] > 5
+    next if distortion_used[parts[3]] > 5
+
+    # Skip if folder is filled already
+    next if already_taken.size > IMAGE_FOLDER_SIZE
 
     # Create a folder
-    new_folder = Dir.mkdir("output/#{i}")
+    new_path = "output/#{i}"
+    FileUtils.mkpath(new_path) unless File.directory?(new_path)
 
     # Move file into a folder
-    FileUtils.move file, new_folder
+    FileUtils.move file, new_path
 
     # Update arrays and hashes
-    already_taken << parts[1]
-    distortion_used[part[2]] += 1
+    already_taken << parts[2]
+    distortion_used[parts[3]] += 1
   end
 end
